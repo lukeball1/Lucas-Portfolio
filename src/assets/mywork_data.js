1e8a658bb3691ffbfff6d4a735af06d75443445d
Lucas Gehner <lmggxf@umsystem.edu>
2025-09-01 17:08:16 -0500

    added ACM Web project description. Could use work

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -6,13 +6,17 @@ const mywork_data = [
         w_no:1,
         w_name:"Slotify",
         w_img:slotify_img,
-        w_description:"This was my team's first hackathon project, as well as our first time working with react. This project focused on building a website where the user could enter in a Spotify playlist, and then guess the song name wordle style based on the sound snippet that plays at the beginning of each round. "
+        w_description:"This was my team's first hackathon project, as well as our first time working with react. This project focused on building a website where the user could enter in a Spotify playlist, and then guess the song name wordle style based on the sound snippet that plays at the beginning of each round. ",
+        w_github: "https://github.com/lukeball1/Pickhacks-Project",
+        w_devpost: "https://devpost.com/software/slotify"
     },
     {
         w_no:2,
         w_name:"ACM Web Website",
         w_img:ACMWebsite_img,
-        w_description:"null"
+        w_description:"This is the ACM Web website, created by members of ACM Web. As the Chair, part of my duty is to ensure the website is up-to-date and running for the club. This website was developed using Astro, a React-based framework. ",
+        w_github:"https://github.com/sigdotcom/acmweb-site",
+        w_devpost: null
     }
 ]
 
