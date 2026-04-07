107a97498d668b5fff48fc25e2a67709d96ffd1a
Lucas <lmggxf@umsystem.edu>
2026-04-07 11:16:13 -0500

    updated images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Hero.css'
-import profile_img from '../../assets/croppedMeInSuit.png'
+import profile_img from '../../assets/cropped-headshot.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
